8a2f4e034d6fc62f9a5da48f9a471b9e6857c76e
Carlos <carlosokumu254@gmail.com>
2023-03-11 04:53:28 -0500

    [UPDATE]

--- app/src/main/java/com/carlos/netter/MainActivity.kt
+++ app/src/main/java/com/carlos/netter/MainActivity.kt
@@ -3,40 +3,46 @@ package com.carlos.netter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.carlos.netter.ui.theme.NetterTheme
-import java.lang.reflect.Modifier
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.carlos.netter.theme.Black
+import com.carlos.netter.theme.Blue200
+import com.carlos.netter.theme.NetterTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.fetchCurrencies()
+
+
+
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
+
+            val color = if (isSystemInDarkTheme()) Black else Blue200
+
+            DisposableEffect(systemUiController, useDarkIcons) {
+                // Update all of the system bar colors to be transparent, and use
+                // dark icons if we're in light theme
+                systemUiController.setSystemBarsColor(
+                    color = color,
+                    darkIcons = useDarkIcons
+                )
+                onDispose {}
+            }
             NetterTheme {
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
+                NetterApp(mainActivityViewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-   // Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NetterTheme {
-        Greeting("Android")
     }
 }
