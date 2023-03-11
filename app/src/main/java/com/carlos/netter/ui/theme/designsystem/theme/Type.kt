b83a838c70650e711708f6199712577ea6c1ea46
Carlos <carlosokumu254@gmail.com>
2023-03-11 02:32:12 -0500

    [UPDATE]Migrated to material 3 typography

---
+++ app/src/main/java/com/carlos/netter/ui/theme/designsystem/theme/Type.kt
@@ -0,0 +1,54 @@
+package com.carlos.netter.ui.theme
+
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.carlos.netter.R
+
+
+private val Poppins = FontFamily(
+    Font(R.font.poppin_bold, FontWeight.W500),
+    Font(R.font.poppin_light, FontWeight.W600),
+    Font(R.font.poppins_medium, FontWeight.W400)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displayMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+
+
+    )
